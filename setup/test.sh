281648f3e6c00f3eb3b7ca45172cdfd1811cba55
Nick De Cooman <nick@nickdecooman.com>
2017-05-26 14:45:19 +0200

    Improve general setup

--- setup/test.sh
+++ setup/test.sh
@@ -2,5 +2,7 @@
 
 . .env.system
 
+# TODO: Change to docker-compose run in case container is not running
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker-compose -p ${NAME} -f ${DIR}/dc.dev.yml exec node yarn test
--- setup/up.sh
+++ setup/up.sh
@@ -4,7 +4,7 @@ export ENV=${1:-prod}
 
 . .env.system
 
-printf "\nRunning version ${VERSION} in ${ENV} mode \n\n"
+printf "\nRunning version ${VERSION} in ${ENV} mode on port ${PORT} \n\n"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker-compose -p ${NAME} -f ${DIR}/dc.$ENV.yml up
