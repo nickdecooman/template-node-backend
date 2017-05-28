4d61da21a1d55be0fd5a179d277d4a4a72367c85
Nick De Cooman <nick@nickdecooman.com>
2017-05-28 18:09:07 +0200

    Do more cleanup for production builds

--- app/build.sh
+++ app/build.sh
@@ -4,7 +4,7 @@ if [ "$NODE_ENV" == "production" ]; then
   echo "Building in production mode"
   NODE_ENV=development yarn install;
   ./node_modules/.bin/babel ./src -d build
-  rm -rf src .babelrc .flowconfig
+  rm -rf src .babelrc .flowconfig .eslintrc .eslintignore __mocks__ __tests__ yarn.lock package.json
   rm -- "$0"
 else
   echo "Building in development mode"
